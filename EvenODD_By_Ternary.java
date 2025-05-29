faafdec4f5055544cfd66308a0ff3baca84c4649
aakashownprep <aakashownprep@gmail.com>
2025-05-29 10:42:57 +0530

    Add files via upload

---
+++ EvenODD_By_Ternary.java
@@ -0,0 +1,7 @@
+class EvenODD_By_Ternary{
+    public static void main(String[] args) {
+        int number=8;
+        String result=(number % 2 == 0)?"even_number":"odd_number";
+        System.out.println("Number is = "+result);
+    }
+}
